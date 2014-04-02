e970a1805de492b0b6f318e44ed3a3a0eb5c92bd
Idrees Khan <idrees.x.khan@gmail.com>
2014-04-01 21:30:41 -0400

    Initial commit.
    - Adding project files for Activite using Scala
    - Adding MIT license

---
+++ src/com/khan/app/activite/MainActivity.scala
@@ -0,0 +1,15 @@
+package com.khan.app.activite
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    override def onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
